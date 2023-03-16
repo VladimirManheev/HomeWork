60185cb4df9f379a25420c9a5ac503a95679a9eb
VladimirManheev <126929220+VladimirManheev@users.noreply.github.com>
2023-03-16 10:07:36 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,13 @@
-﻿ // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
 
 Console.Clear();
 Console.Write("Введите число: ");
-int n1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число: ");
-int n2 = Convert.ToInt32(Console.ReadLine());
-if (n1 > n2)
-    Console.Write(n1);
-else 
-    Console.Write(n2);
- 
-
+int n = Convert. ToInt32(Console.ReadLine());
+for(int i = 1; i <= n; i++)
+{
+if (i % 2 == 0)
+Console.Write(i + ", ");
+}
